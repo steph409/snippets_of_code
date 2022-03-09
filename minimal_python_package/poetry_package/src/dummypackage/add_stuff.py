bf79bb2a3045b1d05486aae66088f2274ac3ed9c
stephi <stephanie.lanius@ginkgo-analytics.com>
2022-03-09 10:08:01 +0100

    (feat) add dummy package code

---
+++ minimal_python_package/poetry_package/src/dummypackage/add_stuff.py
@@ -0,0 +1,8 @@
+def add_stuff(a, b):
+    """
+    Placeholder function that allows you to add two numbers even with a broken '+' key.
+    :param a: First Number
+    :param b: Second Number
+    :return: The Sum
+    """
+    return a + b
